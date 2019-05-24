e64fafe00fa7d1c47ae069945cf664232a759ab2
SeriousWeasle <seriousweasle@gmail.com>
2019-05-25 01:10:33 +0200

    uploading progress

--- server.js
+++ server.js
@@ -27,7 +27,64 @@ io.on('connection', function(socket) {
         console.log(data);
     });
     socket.on("click", function () {
-        console.log('click');
+        console.log('click @' + socket.id);
         socket.emit("clickres");
-    })
+    });
+
+    socket.on('requestTypes', function() {
+        socket.emit('convtypes', {out:converters});
+    });
+
+    socket.on('requestDistConv', function(data) {
+        socket.emit('returnDist', convertdist(data.val_in, data.fact_in, data.unit_in, data.fact_out, data.unit_out));
+    });
 });
+
+
+// Converter types
+const converters = ['dist', 'time', 'mass', 'vel'];
+const units = {
+    dist:
+    {
+        meter: 1,
+        foot: 0.3048,
+        inch: 0.0254,
+        yard: 0.9144,
+        nmile: 1852,
+        mile: 1609.34,
+        banana: 0.18
+    },
+    time:
+    {
+        second: 1,
+        minute: 0.0166667,
+        hour: 0.00027777833333,
+        day: 1.157409722208333465e-5,
+        week: 1.653442460297619205e-6,
+        month: 3.805178478297558514e-7,
+        year: 3.170985540296803748e-8,
+        shrek: 1.7543859649122807017543859649123e-4
+    },
+    mass:
+    {
+        gram: 1,
+        impton: 9.8421e-7,
+        uston: 1.10231520208e-6,
+        stone: 0.000157473,
+        pound: 0.00220462,
+        ounce: 0.035274
+    },
+    vel:
+    {
+        meterpersecond: 1,
+        knot: 1.94384
+    }
+};
+
+function convertdist (val_in, fact_in, unit_in, fact_out, unit_out) {
+    mult_in = units.dist[unit_in];
+    mult_out = units.dist[unit_out];
+
+    output = (val_in * fact_in * mult_in) / (fact_out * mult_out);
+    return {type: 'dist', val: output};
+}
--- static/client.js
+++ static/client.js
@@ -1,12 +1,36 @@
 socket = io();
 
+const dropdown_mults = "<option value=0.000001>µ</option><option value =0.001>m</option><option selected value=1></option><option value=1000>k</option><option value=1000000>M</option><option value=1000000000>G</option>";
+
 window.onclick = function (event){
     if (event.button == 0) {
         socket.emit("click");
-        console.log(event);
     }
 }
 
 socket.on('clickres', function() {
     console.log("response from server");
 });
+
+function switchConverter(type){
+    console.log(type);
+}
+
+function convertDistance () {
+
+}
+
+socket.on('convtypes', function(data) {
+    for (type in data.out) {
+        document.getElementById(data.out[type] + "_mult_out").innerHTML = dropdown_mults;
+        document.getElementById(data.out[type] + "_mult_in").innerHTML = dropdown_mults;
+    }
+});
+
+var testInput = {val_in:10, fact_in:1, unit_in:"meter", fact_out:1, unit_out:"banana"};
+
+socket.on("returnDist", function(data) {
+    console.log(data);
+});
+
+socket.emit('requestTypes');
